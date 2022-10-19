ffc732a3ccf5495e2318a9f8ff152c4b9f116d73
lera <105740475+yakovlevalera@users.noreply.github.com>
2022-10-19 16:43:21 +0300

    додає кр

---
+++ кр.js/06.10.js
@@ -0,0 +1,13 @@
+// вставити
+const balance = 1000;
+// let message;
+
+// if(balance>1){
+//     message = "Ваш баланс позитивний!"
+// } else{
+//     message = "Ваш баланс негативний!"
+// }
+
+const message = balance > 1 ? "Ваш баланст позитивний!" : "Ваш баланс негатиний!";
+
+console.log(message);
