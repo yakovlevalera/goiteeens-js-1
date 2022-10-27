b71ecb8fdf165418bf8b3e6121479f686194ebe1
lera <105740475+yakovlevalera@users.noreply.github.com>
2022-10-27 19:25:50 +0300

    дадає дз 4

--- дз.js/homework-4.js
+++ дз.js/homework-4.js
@@ -1,47 +1,47 @@
-// 1 завдання
+// // 1 завдання
 
 
-let num = 1;
+// let num = 1;
 
-while (num <= 10){
-    console.log(num);
-    num += 1;
-}
-
-
-// 2 завдання
+// while (num <= 10){
+//     console.log(num);
+//     num += 1;
+// }
 
 
-for (let i = 0; i <= 10; i+=1) {
-    if (i % 2 !== 0){
-        console.log('Число не парне');
-    } else {
-        console.log('Число парне');
-    }
-}
+// // 2 завдання
 
 
-// 3 завдання
+// for (let i = 0; i <= 10; i+=1) {
+//     if (i % 2 !== 0){
+//         console.log('Число не парне');
+//     } else {
+//         console.log('Число парне');
+//     }
+// }
 
 
-let number = 20;
-
-while (number >= 28) {
-    number += 1;
-    console.log(number);
-}
+// // 3 завдання
 
 
-// 4 завдання
+// let number = 20;
+
+// while (number >= 28) {
+//     number += 1;
+//     console.log(number);
+// }
 
 
-let name = "";
-while (name.length < 6) {
-    name = prompt(`Напишіть ім'я вашого улюбленого героя`);
-    if(name.lenght < 6){
-        console.log(`${name} - виводити`);
-    } else{
-        console.log('Завершити');
-            break;
-    }
-}
+// // 4 завдання
+
+
+// let name = "";
+// while (name.length < 6) {
+//     name = prompt(`Напишіть ім'я вашого улюбленого героя`);
+//     if(name.lenght < 6){
+//         console.log(`${name} - виводити`);
+//     } else{
+//         console.log('Завершити');
+//             break;
+//     }
+// }
