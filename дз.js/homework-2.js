52b462c0d503de6667a9b4f032b347c02053fabf
lera <105740475+yakovlevalera@users.noreply.github.com>
2022-10-27 19:25:38 +0300

    дадає дз 2

--- дз.js/homework-2.js
+++ дз.js/homework-2.js
@@ -1,77 +1,77 @@
-// 1 завдання
+// // 1 завдання
  
  
-const k1 = Number.parseInt('5px');
-console.log(k1);
+// const k1 = Number.parseInt('5px');
+// console.log(k1);
  
-const k2 = Number.parseInt('12djd334');
-console.log(k2);
+// const k2 = Number.parseInt('12djd334');
+// console.log(k2);
  
-const k3 = Number.parseInt ('12.45asdwe12');
-console.log(k3);
+// const k3 = Number.parseInt ('12.45asdwe12');
+// console.log(k3);
  
-const k4 = Number.parseInt('qwqweeewq');
-console.log(k4);
- 
- 
-// 2 завдання
+// const k4 = Number.parseInt('qwqweeewq');
+// console.log(k4);
  
  
-const k5 = Number.parseFloat('5px');
-console.log(k5);
- 
-const k6 = Number.parseFloat('12djd334');
-console.log(k6);
- 
-const k7 = Number.parseFloat ('12.45asdwe12');
-console.log(k7);
- 
-const k8 = Number.parseFloat('qwqweeewq');
-console.log(k8);
+// // 2 завдання
  
  
-// 3 завдання
+// const k5 = Number.parseFloat('5px');
+// console.log(k5);
+ 
+// const k6 = Number.parseFloat('12djd334');
+// console.log(k6);
+ 
+// const k7 = Number.parseFloat ('12.45asdwe12');
+// console.log(k7);
+ 
+// const k8 = Number.parseFloat('qwqweeewq');
+// console.log(k8);
  
  
-console.log(Math.max(2,34,99,3,22,36,733,18));
-console.log(Math.min(2,34,99,3,22,36,733,18));
+// // 3 завдання
  
  
-// 4 завдання
+// console.log(Math.max(2,34,99,3,22,36,733,18));
+// console.log(Math.min(2,34,99,3,22,36,733,18));
  
  
-console.log(Math.random() * (19 - 3) + 3);
+// // 4 завдання
  
  
-// 5 завдання
+// console.log(Math.random() * (19 - 3) + 3);
  
  
-const result = 5 + 5 + '5';
-console.log(result);
-console.log(typeof result);
+// // 5 завдання
  
  
-// 6 завдання
+// const result = 5 + 5 + '5';
+// console.log(result);
+// console.log(typeof result);
  
  
-const email = 'yakovlevalera2507@gmail.com' ;
-console.log(email.includes('@'));
-console.log(email.length);
+// // 6 завдання
  
  
-// 7 завдання
+// const email = 'yakovlevalera2507@gmail.com' ;
+// console.log(email.includes('@'));
+// console.log(email.length);
  
  
-const my = 'My';
-const nAme = 'name';
-const is = 'is';
-const fullName = `${my} ${nAme} ${is} Victor.`;
-console.log(fullName);
+// // 7 завдання
  
  
-// 8 завдання
+// const my = 'My';
+// const nAme = 'name';
+// const is = 'is';
+// const fullName = `${my} ${nAme} ${is} Victor.`;
+// console.log(fullName);
  
  
-const userName = 'Валеріє';
-const payment = 1500;
-alert(`Дякуємо, ${userName}! До сплати ${payment} гривень.`);
+// // 8 завдання
+ 
+ 
+// const userName = 'Валеріє';
+// const payment = 1500;
+// alert(`Дякуємо, ${userName}! До сплати ${payment} гривень.`);
