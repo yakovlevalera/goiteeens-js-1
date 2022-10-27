644de05f57851b8808fb60274cbab39defc1b177
lera <105740475+yakovlevalera@users.noreply.github.com>
2022-10-27 19:25:44 +0300

    дадає дз 3

--- дз.js/homework-3.js
+++ дз.js/homework-3.js
@@ -1,79 +1,79 @@
-// 1 завдання
+// // 1 завдання
 
 
-const random = prompt('Введіть число від 55 до 99 включно');
+// const random = prompt('Введіть число від 55 до 99 включно');
 
-if (random >= 55 && random <= 99) {
-    alert ('Число потрапляє в діапазон');
-} 
+// if (random >= 55 && random <= 99) {
+//     alert ('Число потрапляє в діапазон');
+// } 
 
-else{
-    alert ('Число не потрапляє в діапазон')
-}
-
-
-// 2 завдання
+// else{
+//     alert ('Число не потрапляє в діапазон')
+// }
 
 
-const age = 55;
-
-if (age > 0 && age < 16){
-    console.log('Діти');
-} else if(age > 17 && age < 60){
-    console.log('Дорослі');
-} else if(age > 61 && age < 100){
-    console.log('Пенсіонери');
-}
-
-// 3 завдання
+// // 2 завдання
 
 
-const firstname = 'Lera';
-const secondname = 'Yakovleva';
+// const age = 55;
 
-if(firstname.length > 4 && secondname.length > 5 ) {
-    console.log(firstname.length + secondname.length);
-} else{
-    alert ('УПС') 
-}
+// if (age > 0 && age < 16){
+//     console.log('Діти');
+// } else if(age > 17 && age < 60){
+//     console.log('Дорослі');
+// } else if(age > 61 && age < 100){
+//     console.log('Пенсіонери');
+// }
 
-
-// 4 завдання
-
-
-const NUMBER = Math.random() * (5 - 1) + 1;
-const randomnumber = parseInt(NUMBER);
-
-if (randomnumber === 1) {
-    console.log('Випало число 1');
-}else if (randomnumber === 2){
-    console.log('Випало число 2');
-}else if (randomnumber === 3){
-    console.log('Випало число 3');
-}else if (randomnumber === 4){
-    console.log('Випало число 4');
-}else{
-    console.log('Випало число 5');
-}
+// // 3 завдання
 
 
-// 5 завдання
+// const firstname = 'Lera';
+// const secondname = 'Yakovleva';
+
+// if(firstname.length > 4 && secondname.length > 5 ) {
+//     console.log(firstname.length + secondname.length);
+// } else{
+//     alert ('УПС') 
+// }
 
 
-const lang = prompt ('Оберіть мову');
-switch (lang) {
-    case 'ua':
-        console.log('Жовтень');
-        break;
-    case 'en':
-        console.log('October');
-        break;
-    case 'ru':
-        console.log('Октябрь');
-        break;
-    case 'fr':
-        console.log('Octobre');
-        break;
-    default: console.log(lang);
-        break;
-}
+// // 4 завдання
+
+
+// const NUMBER = Math.random() * (5 - 1) + 1;
+// const randomnumber = parseInt(NUMBER);
+
+// if (randomnumber === 1) {
+//     console.log('Випало число 1');
+// }else if (randomnumber === 2){
+//     console.log('Випало число 2');
+// }else if (randomnumber === 3){
+//     console.log('Випало число 3');
+// }else if (randomnumber === 4){
+//     console.log('Випало число 4');
+// }else{
+//     console.log('Випало число 5');
+// }
+
+
+// // 5 завдання
+
+
+// const lang = prompt ('Оберіть мову');
+// switch (lang) {
+//     case 'ua':
+//         console.log('Жовтень');
+//         break;
+//     case 'en':
+//         console.log('October');
+//         break;
+//     case 'ru':
+//         console.log('Октябрь');
+//         break;
+//     case 'fr':
+//         console.log('Octobre');
+//         break;
+//     default: console.log(lang);
+//         break;
+// }
