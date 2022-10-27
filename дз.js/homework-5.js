0e824e79ba28afda3646a2f73e3977bae71028ff
lera <105740475+yakovlevalera@users.noreply.github.com>
2022-10-27 13:32:39 +0300

    додає дз 5

---
+++ дз.js/homework-5.js
@@ -0,0 +1,83 @@
+// завдання 1
+
+const Arr1 = [1, 5, '4', 'hello'];
+
+const Arr2 = [true, 2, {}, ['a'], 222];
+
+const arrSum = Arr1[2] + Arr2[2];
+
+consol.log(Arr1[2], Arr2[2], arrSum)
+
+// завдання 2
+
+const arr1 = [1, 5, "4", "hello"];
+
+console.log(arr1);
+
+arr1.push(22);
+
+console.log(arr1);
+
+// завдання 3
+
+for (const Arr of Arr2) {
+   console.log(typeof Arr);
+}
+
+// завдання 4
+
+const message = "Welcome to Ukraine!";
+
+const arrMessage = message.split(' ');
+
+console.log(arrMessage);
+
+console.log(arrMessage.indexOf('l'));
+
+const messageStr = arrMessage.join();
+
+console.log(messageStr);
+
+// завдання 5
+
+const styles = ['Джаз', 'Блюз'];
+
+styles.push('Рок-н-ролл');
+
+styles.splice(1, 1, 'Класика');
+
+styles.shift(0);
+
+console.log(0);
+
+styles.unshift('Реп', 'Реггі');
+
+console.log(styles);
+
+// завдання 6
+
+const country = prompt ('Введіть назву країни');
+
+switch (country) {
+    case 'Китай':
+        console.log(`Доставка в ${country} буде коштувати 100 кредитів`);
+        break;
+    
+    case 'Чилі':
+        console.log(`Доставка в ${country} буде коштувати 250 кредитів`);
+        break;
+
+    case 'Австралія':
+        console.log(`Доставка в ${country} буде коштувати 170 кредитів`);
+        break;
+    
+    case 'Індія':
+        console.log(`Доставка в ${country} буде коштувати 80 кредитів`);
+        break;
+
+    case 'Ямайка':
+        console.log(`Доставка в ${country} буде коштувати 120 кредитів`);
+        break;
+    default:
+        console.log('У вашій країні доставка недоступна');
+}
