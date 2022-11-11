201d7b53e67585ec0d80de14191b3dab15fa3383
lera <105740475+yakovlevalera@users.noreply.github.com>
2022-11-11 09:38:35 +0200

    дз 7

---
+++ дз.js/homework-7.js
@@ -0,0 +1,64 @@
+// 1 завдання
+
+
+const hello1 = function() {
+  return "Привіт, JavaScript";
+}
+
+
+// 2 завдання
+
+
+const hello2 = function(name) {
+  return `Привіт, ${name}`;
+}
+console.log(hello2('Василь'));
+
+
+// 3 завдання
+
+
+const mull = function (n, m) {
+  console.log(n * m);
+  console.log(n + m);
+  console.log(n - m);
+}
+mull(10, 2);
+
+
+// 4 завдання
+
+
+const myNewArrowFunction = function (...args) {
+  console.log(args);
+}
+
+myNewArrowFunction(1, 2, 3);
+myNewArrowFunction(100, 200, 300, 400, 500);
+myNewArrowFunction("Js", "Python", "Java", "PHP", "C++");
+
+
+// 5 завдання
+
+
+const myAverageScore = function (array) {
+  let result = 0;
+
+  for (const number of array) {
+      result = (result + number) / array.length;
+  }
+  
+  if (result > 91 && result < 100) {
+      console.log("A");
+  } else if(result > 81 && result < 90){
+      console.log("B");
+  } else if(result > 71 && result < 80){
+      console.log("C");
+  } else if(result > 0 && result < 70){
+      console.log("D");
+  }
+}
+console.log(myAverageScore([100, 75, 81, 96]));
+// console.log(myAverageScore([45, 63, 85, 70]));
+// console.log(myAverageScore([77, 82, 60, 58]));
+// console.log(myAverageScore([93, 99, 93, 96]));
