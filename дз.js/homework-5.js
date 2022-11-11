6e81bd2658fb6a7514789f3f5b7ec5cb60df374f
lera <105740475+yakovlevalera@users.noreply.github.com>
2022-11-11 09:38:22 +0200

    дз 5

--- дз.js/homework-5.js
+++ дз.js/homework-5.js
@@ -1,83 +1,83 @@
 // завдання 1
 
-const Arr1 = [1, 5, '4', 'hello'];
+// const Arr1 = [1, 5, '4', 'hello'];
 
-const Arr2 = [true, 2, {}, ['a'], 222];
+// const Arr2 = [true, 2, {}, ['a'], 222];
 
-const arrSum = Arr1[2] + Arr2[2];
+// const arrSum = Arr1[2] + Arr2[2];
 
-consol.log(Arr1[2], Arr2[2], arrSum)
+// consol.log(Arr1[2], Arr2[2], arrSum)
 
 // завдання 2
 
-const arr1 = [1, 5, "4", "hello"];
+// const arr1 = [1, 5, "4", "hello"];
 
-console.log(arr1);
+// console.log(arr1);
 
-arr1.push(22);
+// arr1.push(22);
 
-console.log(arr1);
+// console.log(arr1);
 
 // завдання 3
 
-for (const Arr of Arr2) {
-   console.log(typeof Arr);
-}
+// for (const Arr of Arr2) {
+//    console.log(typeof Arr);
+// }
 
 // завдання 4
 
-const message = "Welcome to Ukraine!";
+// const message = "Welcome to Ukraine!";
 
-const arrMessage = message.split(' ');
+// const arrMessage = message.split(' ');
 
-console.log(arrMessage);
+// console.log(arrMessage);
 
-console.log(arrMessage.indexOf('l'));
+// console.log(arrMessage.indexOf('l'));
 
-const messageStr = arrMessage.join();
+// const messageStr = arrMessage.join();
 
-console.log(messageStr);
+// console.log(messageStr);
 
 // завдання 5
 
-const styles = ['Джаз', 'Блюз'];
+// const styles = ['Джаз', 'Блюз'];
 
-styles.push('Рок-н-ролл');
+// styles.push('Рок-н-ролл');
 
-styles.splice(1, 1, 'Класика');
+// styles.splice(1, 1, 'Класика');
 
-styles.shift(0);
+// styles.shift(0);
 
-console.log(0);
+// console.log(0);
 
-styles.unshift('Реп', 'Реггі');
+// styles.unshift('Реп', 'Реггі');
 
-console.log(styles);
+// console.log(styles);
 
 // завдання 6
 
-const country = prompt ('Введіть назву країни');
+// const country = prompt ('Введіть назву країни');
 
-switch (country) {
-    case 'Китай':
-        console.log(`Доставка в ${country} буде коштувати 100 кредитів`);
-        break;
+// switch (country) {
+//     case 'Китай':
+//         console.log(`Доставка в ${country} буде коштувати 100 кредитів`);
+//         break;
     
-    case 'Чилі':
-        console.log(`Доставка в ${country} буде коштувати 250 кредитів`);
-        break;
+//     case 'Чилі':
+//         console.log(`Доставка в ${country} буде коштувати 250 кредитів`);
+//         break;
 
-    case 'Австралія':
-        console.log(`Доставка в ${country} буде коштувати 170 кредитів`);
-        break;
+//     case 'Австралія':
+//         console.log(`Доставка в ${country} буде коштувати 170 кредитів`);
+//         break;
     
-    case 'Індія':
-        console.log(`Доставка в ${country} буде коштувати 80 кредитів`);
-        break;
+//     case 'Індія':
+//         console.log(`Доставка в ${country} буде коштувати 80 кредитів`);
+//         break;
 
-    case 'Ямайка':
-        console.log(`Доставка в ${country} буде коштувати 120 кредитів`);
-        break;
-    default:
-        console.log('У вашій країні доставка недоступна');
-}
+//     case 'Ямайка':
+//         console.log(`Доставка в ${country} буде коштувати 120 кредитів`);
+//         break;
+//     default:
+//         console.log('У вашій країні доставка недоступна');
+// }
